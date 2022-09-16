c98340dbd5952f2067be38ca71b7c31126878f69
VitorWiki <vjbmendes65@gmail.com>
2022-09-16 03:06:41 -0300

    basic structure creation

---
+++ src/test/java/com/alten/hotelBooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.hotelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
